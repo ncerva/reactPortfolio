0b7f152ac054b0a253b3ee303a76e99ef38886a9
Nayeli Cervantes <nayelicervantes40@gmail.com>
2021-06-16 16:43:01 -0500

    portfolio skeleton

--- reactportfolio/src/Components/Projects/Projects.js
+++ reactportfolio/src/Components/Projects/Projects.js
@@ -4,7 +4,17 @@ import "./projects.css"
 export default function Projects() {
     return (
         <div className="projects" id="projects">
-            projects
+            <h1>Portfolio </h1>
+            <ul className="ul">
+                <li className="li">Coding Bootcamp</li>
+                <li className="li">Graphic Design</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+            <img className="img" src="../assets/images/podcastJunkie.png" alt=""></img>
+            <h3 className="h3"> Podcast Junkie</h3>
+            </div>
+            </div>
         </div>
     )
 }
