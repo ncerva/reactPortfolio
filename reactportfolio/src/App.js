252b510b20d9cf8651836ac9c17504d2e3ea8b4e
Nayeli Cervantes <nayelicervantes40@gmail.com>
2021-06-16 11:41:23 -0500

    css linked to js

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,9 +1,23 @@
-
+import React from 'react'
+import Header from "./Components/header/Header";
+import Projects from "./Components/projects/Projects";
+import Footer from "./Components/footer/Footer";
+import Introduction from "./Components/introduction/Introduction";
+import "./app.css"
 
 function App() {
   return (
     <div className="app">
-      Hello
+      <Header/> 
+      <div className="sections">
+      <Introduction/>
+      <div className="sections">
+      <Projects />
+      <div className="sections">
+      </div>
+      <Footer/>
+    </div>
+    </div> 
     </div>
   );
 }
---
+++ reactportfolio/src/Components/introduction/Introduction.js
@@ -0,0 +1,9 @@
+import "./introduction.css"
+
+export default function Introduction() {
+    return (
+        <div className="introduction">
+            introduction
+        </div>
+    )
+}
