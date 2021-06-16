a44799eb573b681931eff7a95d384404e4355806
Nayeli Cervantes <nayelicervantes40@gmail.com>
2021-06-16 17:29:42 -0500

    header looking good

--- reactportfolio/src/Components/Footer/Footer.js
+++ reactportfolio/src/Components/Footer/Footer.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import "./footer.css"
+import { BsFillChatDotsFill, BsEnvelopeOpenFill } from "react-icons/bs";
 
 export default function Footer() {
     return (
         <div className="footer" id="footer">
-            footer
+            <div className="wrapper">
+                <div className="left">
+                    <div className="itemContainer">
+                        <BsFillChatDotsFill className="icon" />
+                        <span> 210.480.3349</span>
+                    </div>
+                    <div className="itemContainer">
+                        <BsEnvelopeOpenFill className="icon" />
+                        <span> nayelicervantes40@gmail.com</span>
+                    </div>
+                    </div>
+        </div>
         </div>
     )
 }
--- reactportfolio/src/Components/Header/Header.js
+++ reactportfolio/src/Components/Header/Header.js
@@ -4,10 +4,9 @@ import { BsFillChatDotsFill, BsEnvelopeOpenFill } from "react-icons/bs";
 export default function Header() {
     return (
         <div className="header" id="header">
-            <div className="wrapper">
+            <div className="wrapper" id="name"> N A Y E L I  C E R V A N T E S</div>
+            <div className="rapper">
                 <div className="left">
-                    <a href="#introduction" className="logo">N.Cerva</a>
-                    </div>
                     <div className="itemContainer">
                         <BsFillChatDotsFill className="icon" />
                         <span> 210.480.3349</span>
@@ -15,6 +14,7 @@ export default function Header() {
                     <div className="itemContainer">
                         <BsEnvelopeOpenFill className="icon" />
                         <span> nayelicervantes40@gmail.com</span>
+                    </div>
                     </div>
         </div>
         </div>
